3f8c1cc0eb4cd5b1a803cebca936a478429424ed
MatteoLucerni <matteo.lucerni@kelony.com>
2025-04-08 20:55:49 +0200

    feat: lambda - expression bodied members exercise

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LambdaExpressions
+{
+    public class Program
+    {
+
+        private string name = "";
+
+        public string Name
+        {
+            get => char.ToUpper(name[0]) + name[1..];
+
+            set => name = value;
+        }
+
+        public Program(string name) => this.name = name;
+
+        static void Main(string[] args)
+        {
+            Program n = new("matteo");
+            Console.WriteLine(n.Name);
+        }
+    }
+}
